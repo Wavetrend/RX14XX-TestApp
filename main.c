f95705ea3b58f8a588ab160d60aa24f283a1f42c
Dave Meehan <dave_meehan@replicated.co.uk>
2019-11-16 15:40:20 +0000

    Define GPIO interfaces

--- main.c
+++ main.c
@@ -114,64 +114,128 @@ DEFINE_ISR(_T5Interrupt, _T5IF, 5);
 
 DEFINE_TIMER(millisecond_timer, _T5Interrupt, PR5, 0x399, T5CON, T5CONbits.TCKPS, 3, T5CONbits.TON, _T5IE, _T5IF);
 
-///////////////////////////////// LED ///////////////////////////////////////
+///////////////////////////////// GPIO ///////////////////////////////////////
 
 typedef struct {
-    bool (*on)(void * const context);
-    bool (*off)(void * const context);
-} wthal_led_impl_t;
+
+    bool (*set)(void * const, bool const high);
+    bool (*toggle)(void * const);
+    bool (*get)(void * const context);
+    bool (*input)(void * const context, bool const input);
+    bool (*analogue)(void * const context, bool const analogue);
+    bool (*weak_pull_up)(void * const context, bool const enable);
+    bool (*weak_pull_down)(void * const context, bool const enable);
+    bool (*output_drain)(void * const context, bool const enable);
+    
+} wthal_gpio_impl_t;
 
 typedef struct {
     
-    wthal_led_impl_t * impl;
-    void * const context;
+    wthal_gpio_impl_t * impl;
+    void * context;
     
-} wthal_led_t;
+} wthal_gpio_t;
 
-wthal_led_t * const wthal_led_init(wthal_led_t * const self, wthal_led_impl_t * const impl, void * const context) {
+wthal_gpio_t * const wthal_gpio_init(wthal_gpio_t * const self, wthal_gpio_impl_t * const impl, void * const context) {
     self->impl = impl;
+    self->context = context;
     return self;
 }
 
-bool wthal_led_on(wthal_led_t * const self) {
-    return self->impl->on(self->context);
+bool wthal_gpio_set(wthal_gpio_t * const self, bool const high) {
+    return self->impl->set(self->context, high);
 }
 
-bool wthal_led_off(wthal_led_t * const self) {
-    return self->impl->off(self->context);
+bool wthal_gpio_toggle(wthal_gpio_t * const self) {
+    return self->impl->toggle(self->context);
 }
 
-#define DEFINE_LED(NAME, TRIS, LAT, WPU, WPD, ODD) \
+bool wthal_gpio_get(wthal_gpio_t * const self) {
+    return self->impl->get(self->context);
+}
+
+bool wthal_gpio_input(wthal_gpio_t * const self, bool const input) {
+    return self->impl->input(self->context, input);
+}
+
+bool wthal_gpio_analogue(wthal_gpio_t * const self, bool const analogue) {
+    return self->impl->analogue(self->context, analogue);
+}
+
+bool wthal_gpio_weak_pull_up(wthal_gpio_t * const self, bool const enable) {
+    return self->impl->weak_pull_up(self->context, enable);
+}
+
+bool wthal_gpio_weak_pull_down(wthal_gpio_t * const self, bool const enable) {
+    return self->impl->weak_pull_down(self->context, enable);
+}
+
+bool wthal_gpio_output_drain(wthal_gpio_t * const self, bool const enable) {
+    return self->impl->output_drain(self->context, enable);
+}
+
+#define DEFINE_GPIO(NAME, PORT, TRIS, LAT, ANS, WPU, WPD, ODRAIN) \
     typedef struct { \
-        wthal_led_t led; \
+        wthal_gpio_t gpio; \
     } NAME ## _t; \
-    static bool NAME ## _on_impl(void * const context) { \
-        LAT = 1; \
+    static bool NAME ## _set_impl(void * const context, bool const high) { \
+        LAT = high; \
         return true; \
     } \
-    static bool NAME ## _off_impl(void * const context) { \
-        LAT = 0; \
+    static bool NAME ## _toggle_impl(void * const context) { \
+        return LAT ^= 1; \
+    } \
+    static bool NAME ## _get_impl(void * const context) { \
+        return PORT; \
+    } \
+    static bool NAME ## _input_impl(void * const context, bool const input) { \
+        TRIS = input; \
         return true; \
     } \
-    static wthal_led_impl_t NAME ## _impl = { \
-        .on = NAME ## _on_impl, \
-        .off = NAME ## _off_impl, \
+    static bool NAME ## _analogue_impl(void * const context, bool const analogue) { \
+        ANS = analogue; \
+        return true; \
+    } \
+    static bool NAME ## _weak_pull_up_impl(void * const context, bool const enabled) { \
+        WPU = enabled; \
+        return true; \
+    } \
+    static bool NAME ## _weak_pull_down_impl(void * const context, bool const enabled) { \
+        WPD = enabled; \
+        return true; \
+    } \
+    static bool NAME ## _output_drain_impl(void * const context, bool const enabled) { \
+        ODRAIN = enabled; \
+        return true; \
+    } \
+    static wthal_gpio_impl_t NAME ## _impl = { \
+        .set = NAME ## _set_impl, \
+        .toggle = NAME ## _toggle_impl, \
+        .get = NAME ## _get_impl, \
+        .input = NAME ## _input_impl, \
+        .analogue = NAME ## _analogue_impl, \
+        .weak_pull_up = NAME ## _weak_pull_up_impl, \
+        .weak_pull_down = NAME ## _weak_pull_down_impl, \
+        .output_drain = NAME ## _output_drain_impl, \
     }; \
-    wthal_led_t * const NAME ## _init(NAME ## _t * const self) { \
+    wthal_gpio_t * const NAME ## _init(NAME ## _t * const self) { \
         TRIS = 0; \
         LAT = 0; \
+        ANS = 0; \
         WPU = 0; \
         WPD = 0; \
-        ODD = 0; \
-        return wthal_led_init(&self->led, &NAME ## _impl, self); \
+        ODRAIN = 0; \
+        return wthal_gpio_init(&self->gpio, &NAME ## _impl, self); \
     }
 
-DEFINE_LED(wt_rx14xx_led1, _TRISD7, _LATD7, _CN16PDE, _CN16PUE, _ODD7);
-DEFINE_LED(wt_rx14xx_led2, _TRISD6, _LATD6, _CN15PDE, _CN15PUE, _ODD6);
+DEFINE_GPIO(wt_rx14xx_led1, _RD7, _TRISD7, _LATD7, _ANSD7, _CN16PUE, _CN16PDE, _ODD7 )
+DEFINE_GPIO(wt_rx14xx_led2, _RD6, _TRISD6, _LATD6, _ANSD6, _CN15PUE, _CN15PDE, _ODD6 )
+DEFINE_GPIO(wt_rx14xx_xpc_reset, _RB2, _TRISB2, _LATB2, _ANSB2, _CN4PUE, _CN4PDE, _ODB2 )
 
 int app_main(
-    wthal_led_t * const startup_led,
-    wthal_led_t * const activity_led
+    wthal_gpio_t * const startup_led,
+    wthal_gpio_t * const activity_led,
+    wthal_gpio_t * const xpc_reset
 ) {
         // initialize the device
     SYSTEM_Initialize();
@@ -179,22 +243,22 @@ int app_main(
         
     TMR5_SoftwareCounterClear();
     TMR5_Start();
-    wthal_led_on(startup_led);
+    wthal_gpio_set(startup_led, true);
     while (TMR5_SoftwareCounterGet() < 5) {
         ClrWdt();
     }
-    wthal_led_off(startup_led);
+    wthal_gpio_set(startup_led, false);
     TMR5_Stop();
     
     while (1)
     {
         // Add your application code
-        wthal_led_on(activity_led);
+        wthal_gpio_set(activity_led, true);
         for (uint32_t i=0 ; i < 100000 ; i++) {
             Nop();
         }
         ClrWdt();
-        wthal_led_off(activity_led);
+        wthal_gpio_set(activity_led, false);
         for (uint32_t i=0 ; i < 100000 ; i++) {
             Nop();
         }
@@ -211,10 +275,15 @@ int main(void)
 {
     wt_rx14xx_led1_t startup_led;
     wt_rx14xx_led2_t activity_led;
+    wt_rx14xx_xpc_reset_t xpc_reset;
+    wthal_gpio_t * const p_startup_led = wt_rx14xx_led1_init(&startup_led);
+    wthal_gpio_t * const p_activity_led = wt_rx14xx_led2_init(&activity_led);
+    wthal_gpio_t * const p_xpc_reset = wt_rx14xx_xpc_reset_init(&xpc_reset);
     
     return app_main(
-        wt_rx14xx_led1_init(&startup_led),
-        wt_rx14xx_led2_init(&activity_led)
+        p_startup_led,
+        p_activity_led,
+        p_xpc_reset
     );
 
 }
