31f650749a65a507088c38b1f61cd3af6b37ff60
Dave Meehan <dave_meehan@replicated.co.uk>
2020-02-22 12:40:21 +0000

    Remove MCC

--- main.c
+++ main.c
@@ -49,7 +49,6 @@
 #include <stdbool.h>
 
 #include <xc.h>
-#include "mcc_generated_files/system.h"
 #include "ezbl_integration/ezbl.h"
 
 #include "wt_assert.h"
@@ -183,6 +182,35 @@ typedef struct {
   
 } wt_rx1400_hal_t;
 
+// CONFIG3
+#pragma config WPFP = WPFP255    //Write Protection Flash Page Segment Boundary->Highest Page (same as page 170)
+#pragma config SOSCSEL = EC    //Secondary Oscillator Power Mode Select->External clock (SCLKI) or Digital I/O mode(
+#pragma config WUTSEL = LEG    //Voltage Regulator Wake-up Time Select->Default regulator start-up time is used
+#pragma config ALTPMP = ALPMPDIS    //Alternate PMP Pin Mapping->EPMP pins are in default location mode
+#pragma config WPDIS = WPDIS    //Segment Write Protection Disable->Segmented code protection is disabled
+#pragma config WPCFG = WPCFGDIS    //Write Protect Configuration Page Select->Last page (at the top of program memory) and Flash Configuration Words are not write-protected
+#pragma config WPEND = WPENDMEM    //Segment Write Protection End Page Select->Protected code segment upper boundary is at the last page of program memory; the lower boundary is the code page specified by WPFP
+
+// CONFIG2
+#pragma config POSCMOD = XT    //Primary Oscillator Select->XT Oscillator mode is selected
+#pragma config IOL1WAY = OFF    //IOLOCK One-Way Set Enable->The IOLOCK bit can be set and cleared as needed, provided the unlock sequence has been completed
+#pragma config OSCIOFNC = ON    //OSCO Pin Configuration->OSCO/CLKO/RC15 functions as port I/O (RC15)
+#pragma config FCKSM = CSECMD    //Clock Switching and Fail-Safe Clock Monitor->Clock switching is enabled, Fail-Safe Clock Monitor is disabled
+#pragma config FNOSC = PRIPLL    //Initial Oscillator Select->Primary Oscillator with PLL module (XTPLL, HSPLL, ECPLL)
+#pragma config PLL96MHZ = ON    //96MHz PLL Startup Select->96 MHz PLL is enabled automatically on start-up
+#pragma config PLLDIV = DIV2    //96 MHz PLL Prescaler Select->Oscillator input is divided by 2 (8 MHz input)
+#pragma config IESO = OFF    //Internal External Switchover->IESO mode (Two-Speed Start-up) is disabled
+
+// CONFIG1
+#pragma config WDTPS = PS4096    //Watchdog Timer Postscaler->1:4096
+#pragma config FWPSA = PR32    //WDT Prescaler->Prescaler ratio of 1:32
+#pragma config WINDIS = OFF    //Windowed WDT->Standard Watchdog Timer enabled,(Windowed-mode is disabled)
+#pragma config FWDTEN = ON    //Watchdog Timer->Watchdog Timer is enabled
+#pragma config ICS = PGx2    //Emulator Pin Placement Select bits->Emulator functions are shared with PGEC2/PGED2
+#pragma config GWRP = OFF    //General Segment Write Protect->Writes to program memory are allowed
+#pragma config GCP = OFF    //General Segment Code Protect->Code protection is disabled
+#pragma config JTAGEN = OFF    //JTAG Port Enable->JTAG port is disabled
+
 wt_hal_t * const wt_rx1400_hal_init(wt_rx1400_hal_t * const instance, wt_error_t * const error) {
 
   bool ok = true;
@@ -194,6 +222,34 @@ wt_hal_t * const wt_rx1400_hal_init(wt_rx1400_hal_t * const instance, wt_error_t
     wt_rx1400_isr_priority_debug_uart_err = 1,
   };
 
+  //======================================================================
+  // PROCESSOR CLOCK
+  //======================================================================
+
+  // CPDIV 1:1; RCDIV FRC/1; DOZE 1:1; DOZEN disabled; ROI disabled; 
+  CLKDIV = 0x00;
+  // TUN Center frequency; 
+  OSCTUN = 0x00;
+  // ADC1MD enabled; T3MD enabled; T4MD enabled; T1MD enabled; U2MD enabled; T2MD enabled; U1MD enabled; SPI2MD enabled; SPI1MD enabled; T5MD enabled; I2C1MD enabled; 
+  PMD1 = 0x00;
+  // OC5MD enabled; OC6MD enabled; OC7MD enabled; OC8MD enabled; OC1MD enabled; IC2MD enabled; OC2MD enabled; IC1MD enabled; OC3MD enabled; OC4MD enabled; IC6MD enabled; IC7MD enabled; IC5MD enabled; IC8MD enabled; IC4MD enabled; IC3MD enabled; 
+  PMD2 = 0x00;
+  // I2C3MD enabled; PMPMD enabled; U3MD enabled; RTCCMD enabled; CMPMD enabled; CRCMD enabled; I2C2MD enabled; 
+  PMD3 = 0x00;
+  // U4MD enabled; UPWMMD enabled; USB1MD enabled; CTMUMD enabled; REFOMD enabled; LVDMD enabled; 
+  PMD4 = 0x00;
+  // IC9MD enabled; OC9MD enabled; 
+  PMD5 = 0x00;
+  // SPI3MD enabled; 
+  PMD6 = 0x00;
+  // CF no clock failure; NOSC PRIPLL; SOSCEN disabled; POSCEN disabled; CLKLOCK unlocked; OSWEN Switch is Complete; 
+  __builtin_write_OSCCONH((uint8_t) (0x03));
+  __builtin_write_OSCCONL((uint8_t) (0x00));
+
+  //======================================================================
+  // PROGRAMMABLE PIN ASSIGNMENTS
+  //======================================================================
+
   PPS_UNLOCK();
   
   // UART 2 - DEBUG
@@ -201,6 +257,10 @@ wt_hal_t * const wt_rx1400_hal_init(wt_rx1400_hal_t * const instance, wt_error_t
   _RP22R = _RPOUT_U2TX; // RP22/RD3->UART2:U2TX
 
   PPS_LOCK();
+
+  //======================================================================
+  // HAL
+  //======================================================================
 
   ok = !ok ? ok : (instance->hal.system = wt_rx14xx_system_init(&instance->system, error)) != NULL;
 
@@ -260,9 +320,6 @@ int main(void) {
   check_stack();
   uint16_t LastUnusedStackBytes = UnusedStackBytes;
 #endif
-
-  // initialize the device - MUST COME FIRST OR WILL OVERRIDE HAL STYLE CONFIG
-  SYSTEM_Initialize();
 
   bool ok = true;
 
